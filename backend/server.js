a4d38fffc3166aae5051f1adc55066f8383d92ae
Raj Aishwary Pratap Singh <rajaishwaryasingh@gmail.com>
2026-03-10 01:38:12 +0530

    cors error fix

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,11 @@ await connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
+}
+app.use(cors(corsOptions));
 
 // api's
 app.get("/", (req, res) => res.send("Server is Running"));
